f75db2f293088a68b6d5430aa3528ba4c48d9623
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-04 10:07:26 -0300

    Update gasoterapia.controller.js

--- src/controllers/gasoterapia.controller.js
+++ src/controllers/gasoterapia.controller.js
@@ -1,12 +1,12 @@
 const gasoterapiaController = {};
 const Gasoterapia = require('../models/Gasoterapia');
 
-gasoterapiaController.getProducts = async (req,res) => {
+const getProducts = async (req,res) => {
     const products =  await Gasoterapia.find();
     res.json({products})
 }
 
-gasoterapiaController.createProduct = (req,res) => {
+const createProduct = (req,res) => {
     const {nombre, uso, rango, anmat, gases, conector, manual, img } =  req.body;
     new Gasoterapia({
         nombre,
@@ -21,12 +21,12 @@ gasoterapiaController.createProduct = (req,res) => {
     res.json({message: 'New product'})
 }
 
-gasoterapiaController.getProductById = (req,res) => res.json({
+const getProductById = (req,res) => res.json({
     message: 'Product'
 })
 
-gasoterapiaController.deleteProduct = (req,res) => res.json({
+const deleteProduct = (req,res) => res.json({
     message: 'Deleted'
 })
 
-module.exports = gasoterapiaController
+module.exports = {createProduct, getProducts, getProductById, deleteProduct}
