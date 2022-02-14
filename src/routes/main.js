a002c58f6d26080f0c007455dfd1c63887b66ba6
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-02-14 14:22:07 -0300

    Delete frascos model

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,15 +1,10 @@
 const {Router} = require('express');
 const router = Router();
-const gasoterapiaController = require('../controllers/gasoterapia.controller')
-const frascosController = require('../controllers/frascos.controller');
-const accesoriosController = require('../controllers/accesorios.controller');
-const conectoresController = require('../controllers/conectores.controller');
-const usController = require('../controllers/unidadesSuministro.controller')
+
 
 const gasoterapiaRouter = require('./products/gasoterapia');
 const usRouter = require('./products/unidadesSuministro');
 const accesoriosRouter = require('./products/accesorios');
-const frascosRouter = require('./products/frascos');
 const conectoresRouter = require('./products/conectores');
 const modelosRouter = require('./products/modelos');
 
