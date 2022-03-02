a0e18cdaad4c7e1795fc4ba322ae147a54e17fd4
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-02 11:12:29 -0300

    Create form.js

---
+++ src/routes/form.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const router = Router();
+
+
+const controllerContacto = require('../controllers/form/contacto.controller');
+const controllerVenta = require('../controllers/form/venta.controller'); 
+
+
+router.post('/contacto', controllerContacto.postContacto);
+router.post('/venta', controllerVenta.postVenta);
+
+module.exports = router;
