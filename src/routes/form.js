2fb1410d1b9a85eaa6c3e6c493743fb43f046823
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-03-22 16:02:43 -0300

    Update form.js

--- src/routes/form.js
+++ src/routes/form.js
@@ -7,8 +7,9 @@ const controllerVenta = require('../controllers/form/venta.controller');
 
 
 router.post('/contacto', controllerContacto.postContacto);
-router.post('/venta', controllerVenta.postVenta);
+router.post('/venta', controllerVenta.postCotizacion);
 
 router.get('/contacto', controllerContacto.mostrarAlgo)
+router.get('/venta', controllerVenta.mostrarCotizacion)
 
 module.exports = router;
